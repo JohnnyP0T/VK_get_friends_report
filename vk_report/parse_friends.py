4d6f25ad44215042b420416a30aad7e1360d12bc
Johnny pot <arroga077@gmail.com>
2022-08-16 17:31:06 +0700

    refactor

--- vk_report/parse_friends.py
+++ vk_report/parse_friends.py
@@ -1,35 +1,49 @@
 import datetime
 
 
+def parse_friends_country(item_field:any)-> str:
+    if item_field is not None:
+        return item_field.get('title')
+    else:
+        return 'unknown'
+
+def parse_friends_city(item_field:any) -> str:
+    if item_field is not None:
+        return item_field.get('title')
+    else:
+        return 'unknown'
+
+def parse_friends_birth_date(item_field:any) -> str:
+    if item_field is not None:
+        format_date_point_count = item_field.count('.')
+        if format_date_point_count == 2:
+            return datetime.datetime.strptime(item_field, "%d.%m.%Y").isoformat()
+        if format_date_point_count == 1:
+            return datetime.datetime.strptime(item_field, "%d.%m").isoformat()
+        else:
+            return 'unknown'
+    else:
+        return 'unknown'
+
+def parse_friends_sex(item_field:any) -> str:
+    if item_field == 1:
+        return 'female'
+    elif item_field == 2:
+        return 'male'
+    else:
+        return 'unknown'
+
+
 def parse_friends(items:list) -> list:
     person_list = []
     for i in items:
         person_temp = dict()
         person_temp['first_name'] = i.get('first_name')
         person_temp['last_name'] = i.get('last_name')
-        if i.get('country') is not None:
-            person_temp['country'] = i.get('country').get('title')
-        else:
-            person_temp['country'] = 'unknown'
-        if i.get('city') is not None:
-            person_temp['city'] = i.get('city').get('title')
-        else:
-            person_temp['city'] = 'unknown'
-        if i.get('bdate') is not None:
-            format_date_point_count = i.get('bdate').count('.')
-            if format_date_point_count == 2:
-                person_temp['birth_date'] = datetime.datetime.strptime(i.get('bdate'), "%d.%m.%Y").isoformat()
-            if format_date_point_count == 1:
-                person_temp['birth_date'] = datetime.datetime.strptime(i.get('bdate'), "%d.%m").isoformat()
-        else:
-            person_temp['birth_date'] = 'unknown'
-        if i.get('sex') == 1:
-            person_temp['sex'] = 'female'
-        elif i.get('sex') == 2:
-            person_temp['sex'] = 'male'
-        else:
-            person_temp['sex'] = 'unknown'
-
+        person_temp['country'] = parse_friends_country(i.get('country'))
+        person_temp['city'] = parse_friends_city(i.get('city'))
+        person_temp['birth_date'] = parse_friends_birth_date(i.get('bdate'))
+        person_temp['sex'] = parse_friends_sex(i.get('sex'))
         person_list.append(person_temp)
     return person_list
 
