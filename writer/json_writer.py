7a392c7de591176b4ec6b1aac357d21bf4cd0483
JohnnyP0T <arroga077@gmail.com>
2023-07-19 19:18:03 +0700

    fix

--- writer/json_writer.py
+++ writer/json_writer.py
@@ -3,8 +3,8 @@ import json
 
 class WriterJson:
     def __init__(self, indent: int = 4):
-        self.indent = indent
+        self._indent = indent
 
     def write(self, file_name: str, data: list) -> None:
         with open(file_name, 'w', encoding='UTF-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=self.indent)
+            json.dump(data, file, ensure_ascii=False, indent=self._indent)
